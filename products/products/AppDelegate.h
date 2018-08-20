0e62f7d00217b54fbd18ae8678fdc9150874f18a
Apolonio <michaelugale626@gmail.com>
2018-08-20 22:24:46 +0800

    Initial source code
    base project source code

---
+++ products/products/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  products
+//
+//  Created by Michael Ugale on 8/20/18.
+//  Copyright © 2018 Michael Ugale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ products/products/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  products
+//
+//  Created by Michael Ugale on 8/20/18.
+//  Copyright © 2018 Michael Ugale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
